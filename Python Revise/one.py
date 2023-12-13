d8ffc7fec21b5ab5c4df0897757e462911f2ac36
Amulya Kumar Maurya <79034752+Amulya77@users.noreply.github.com>
2023-12-13 23:15:49 +0530

    Add files via upload

---
+++ Python Revise/one.py
@@ -0,0 +1,85 @@
+print("Hello world")
+# print("Hello world")
+# print("Hello world")
+
+#VARIABLES
+a=5
+print(a)
+b=55
+print(b)
+a=6.2
+print(a)
+c=True
+print(c)
+d="Amulya"
+print(d)
+e=None
+print(e)
+
+#TYPE CASTING
+a="1234"
+a=int(a)
+print(type(a))
+a=str(a)
+print(type(a))
+a=float(a)
+print(type(a))
+a=bool(a)
+print(type(a))
+a=None
+print(type(a))
+f="5"
+print(int(f)+1)
+
+
+#OPERATORS
+num1=5
+num2=2
+#print("The value of num1 + num2 is", num1+num2)
+
+#Arithmetic operators
+print("#Arithmetic operators")
+print("The value of num1 + num2 is", num1+num2)
+print("The value of num1 - num2 is", num1-num2)
+print("The value of num1 * num2 is", num1*num2)
+print("The value of num1 / num2 is", num1/num2)
+print("The value of num1 // num2 is", num1//num2)
+print("The value of num1 % num2 is", num1%num2)
+print("The value of num1 ** num2 is", num1**num2)
+
+a=4
+a+=2
+print(a)
+
+#Comparison operators
+print("#Comparison operators")
+x=8
+y=3
+z=4
+print(x>y)
+print(x<y)
+print(x==y)
+print(x>=y)
+print(x<=y)
+print(x!=y)
+
+#Logical operators
+print("#Logical operators")
+print(x<y and x<z)
+print(x<y or x<z)
+print(not x<y)
+print(not x<z)
+print(x==y and x<y)
+
+#Identity operator
+print("#Identity operators")
+x=[1,2,3]
+y=[1,2,3]
+z=x
+print(x is y)
+print(x is z)
+print(y is z)
+print(x is not y)
+
+
+
