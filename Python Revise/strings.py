8dd6a342a11a14d079502be174d95daf1944470b
Amulya Kumar Maurya <79034752+Amulya77@users.noreply.github.com>
2023-12-13 23:16:17 +0530

    Add files via upload

---
+++ Python Revise/strings.py
@@ -0,0 +1,24 @@
+name="amulya"
+print(name)
+
+print(name[0:3]) #starts from 0 and goes till 2 (3-1)
+print(name[0:]) #starts from 0 and goes till end
+print(name[:2]) #starts from 0 goes till 1( 2-1)
+
+print(name.upper())
+print(name.capitalize())
+print(name.lower())
+print(name.count("a"))
+print(name.find("a"))
+print(name.replace("a","b"))
+print(name.replace("a","b",1))
+print(name.replace("a","b",2))
+print(name.replace("a","b",3))
+print(name.isalnum())
+print(name.isalpha())
+print(name.isdigit())
+print(name.isnumeric())
+print(name.islower())
+print(name.isupper())
+print(name.isspace())
+print(name.startswith("a"))
